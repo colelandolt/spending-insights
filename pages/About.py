9970175d3881c5acb710688fb1466fe355a3b34a
Cole Landolt <colelandolt33@gmail.com>
2024-02-04 01:22:07 -0800

    update title and add introduction

--- pages/About.py
+++ pages/About.py
@@ -3,7 +3,7 @@ import streamlit as st
 from src.utils import page_configuration
 
 def main():
-    page_configuration('Transaction Insights')
+    page_configuration('Spending Insights')
 
 if __name__ == '__main__':
     main()
